b907c90cb989b2e8d07fd33e07181825c9930f74
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2024-01-27 13:13:49 +0530

    index completed

--- script.js
+++ script.js
@@ -36,3 +36,20 @@ $('#campaign').owlCarousel({
         }
     }
 });
+$('#project').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+  center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
