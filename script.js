a23083363786453b98e43922190b85ff2c769f93
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2024-02-03 13:24:40 +0530

    about us is add

--- script.js
+++ script.js
@@ -19,6 +19,27 @@ $('#logo').owlCarousel({
         }
     }
 });
+$('#ourteam').owlCarousel({
+    loop:true,
+    autoplay: true,
+      autoplayTimeout: 3000 ,
+    margin:10,
+    animateIn: 'fadeIn' ,
+    animateOut: 'fadeOut',
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
 $('#campaign').owlCarousel({
     loop:true,
     nav:false,
